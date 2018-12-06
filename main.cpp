be8ce607531119078ea9812b8a56e8fa7be7b076
JStanderfer <jjstanderfer@gmail.com>
2018-12-05 23:21:56 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include "HashSC.h"
+#include "BarcodeItem.h"
+#include <string>
+#include <fstream>
+#include <time>
+#include <iostream>
+
+using namespace std;
+int main() {
+	clock_t t;
+	t = clock();
+	string code;
+	//info is the rest of the information 
+	string info;
+	//stores each line before we put it into a BarcodeItem
+	string line;
+	fstream aFile;
+	aFile.open("upc_corpus.txt");
+	//skips the first line
+	string dummy;
+	getline(aFile, dummy);
+
+	HashSC<BarcodeItem> hashTable(1000);
+
+	int i = 0;
+	int j = 10;
+	//change j to the any int to limit the number of items in the tree
+	while (getline(aFile, line) && i < j) {
+		//	try{
+				//make sure we have a UPC code delimited by ','
+		string::size_type loc;
+		//we only care about the first comma
+		loc = line.find(',', 0);
+		code = line.substr(0, loc);
+		info = line.substr(loc + 1);
+
+		BarcodeItem item(code, info);
+		hashTable.insertItem(item.getKey(), item);
+	}
+	cout<<"Enter UPC Code: ";
+	string key;
+	string dummy;
+	cin >> key;
+	//create a dummy barcode item to compare in the list
+	BarcodeItem temp(key, dummy);
+	//note: Ideally we would have a HashSC that extends a scanner so we could use .getKey() inside HashSC. But I ran out of time.
+	//This works because barcodeitem == operator only compares keys
+	hashTable.find(temp);
+
+	aFile.close();
+
+	t = clock() - t;
+	cout << "time: " << t << "milliseconds" << endl;
+	cout << CLOCKS_PER_SEC << "clocks per second" << endl;
+	cout << "time: " << t * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
+}
